0f9df02d71bb861c01649005a788161c51b9d532
Jorge M Guimaraes <jmguimaraes@id.uff.br>
2022-08-18 18:17:27 -0300

    add: region

--- variables.tf
+++ variables.tf
@@ -1,3 +1,9 @@
+variable "default_region" {
+  type        = string
+  default     = "sa-east-1"
+  description = "The region this infrastructure is in"
+}
+
 variable "instance_type" {
   type        = string
   default     = "t2.micro"
