53f76fe1a3314771b42398ec8b790c3ae92f71d3
Jorge M Guimaraes <jmguimaraes@id.uff.br>
2022-08-24 01:24:04 -0300

    add: user owns www

--- modules/ec2/httpd.sh
+++ modules/ec2/httpd.sh
@@ -5,3 +5,7 @@ apt install -y apache2
 systemctl start apache2
 systemctl enable apache2
 echo "<h1>OLA MUNDO DE $(hostname -f)</h1>" > /var/www/html/index.html
+
+adduser ubuntu www-data
+chown -R ubuntu:www-data /var/www
+chmod -R 755 /var/www/html
